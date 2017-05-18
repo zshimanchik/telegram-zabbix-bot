7f0272af8406c1131702e44249b7bf02c20f75e1
Zahar Shimanchik <zahar.shimanchik@yandex.ru>
2017-05-18 12:55:56 +0300

    added csrf_exempt to telegram webhook

--- web/main/views.py
+++ web/main/views.py
@@ -1,8 +1,11 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def telegram_webhook(request):
+    print("got 33smthing: {}".format(request.body))
     return HttpResponse(b"hello world!")
 
 
