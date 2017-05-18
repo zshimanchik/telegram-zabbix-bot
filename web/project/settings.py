682042065afedc650c529d6d2ed99707eea8a6d4
Zahar Shimanchik <zahar.shimanchik@yandex.ru>
2017-05-18 14:47:27 +0300

    logging configuration

--- web/project/settings.py
+++ web/project/settings.py
@@ -125,3 +125,26 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s [%(filename)+15s:%(lineno)-3d] %(levelname)-7s: %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'main': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
