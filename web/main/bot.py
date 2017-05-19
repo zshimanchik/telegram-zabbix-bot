cb01f93cd370863c23b833fa0b713af35dceac03
Zahar Shimanchik <zahar.shimanchik@yandex.ru>
2017-05-19 23:58:58 +0300

    bot commands implemented: /hosts, /items, /last_value

--- web/main/bot.py
+++ web/main/bot.py
@@ -21,6 +21,11 @@ class TelegramZabbixBot:
             Command('/register', self.register, '/register <zabbix_server_ip> <zabbix_user> '
                                                 '<zabbix_password>'),
             Command('/stop', self.stop, '/stop - deleting you from the system'),
+            Command('/hosts', self.hosts, '/hosts - getting list of hosts from zabbix'),
+            Command('/items', self.items, '/items <host_id> - getting list of registered items '
+                                          'for host'),
+            Command('/last_value', self.last_value, '/last_value <item_id> - getting item last '
+                                                    'value')
         ]
 
         self.commands = {x.command: x for x in self.command_list}
@@ -85,6 +90,91 @@ class TelegramZabbixBot:
         user.delete()
         self.api.send_message(user.telegram_id, c.U_WAS_DELETED)
 
+    def hosts(self, user, message):
+        if user.zabbix_host is None:
+            self.api.send_message(user.telegram_id, "You are not registered. Register yourself "
+                                                    "using command /register")
+            return
+        try:
+            api = ZabbixApi(user.zabbix_host, user.zabbix_user, user.zabbix_pass)
+            hosts = api.get_hosts()
+            text = self._list_of_dict_to_str(hosts)
+            self.api.send_message(user.telegram_id, text)
+        except RPCException as ex:
+            self.api.send_message(user.telegram_id, "Can't perform an action at zabbix because of "
+                                                    "the reason: {0}".format(ex.message))
+        except RequestException:
+            self.api.send_message(user.telegram_id, "Can't reach zabbix. Are you sure that it is "
+                                                    "available?")
+        except Exception as ex:
+            LOGGER.exception(ex)
+            self.api.send_message(user.telegram_id, "Can't perform an action at zabbix.")
 
-api = telebot.TeleBot(settings.TELEGRAM_BOT_API)
+    def items(self, user, message):
+        if user.zabbix_host is None:
+            self.api.send_message(user.telegram_id, "You are not registered. Register yourself "
+                                                    "using command /register")
+            return
+        args = (message.text or '').split()
+        if len(args) != 2:
+            self.api.send_message(user.telegram_id, self.commands['/items'].help)
+            return
+
+        try:
+            api = ZabbixApi(user.zabbix_host, user.zabbix_user, user.zabbix_pass)
+            items = api.get_items(args[1])
+            text = self._list_of_dict_to_str(items)
+            self.api.send_message(user.telegram_id, text)
+        except RPCException as ex:
+            self.api.send_message(user.telegram_id, "Can't perform an action at zabbix because of "
+                                                    "the reason: {0}".format(ex.message))
+        except RequestException:
+            self.api.send_message(user.telegram_id, "Can't reach zabbix. Are you sure that it is "
+                                                    "available?")
+        except Exception as ex:
+            LOGGER.exception(ex)
+            self.api.send_message(user.telegram_id, "Can't perform an action at zabbix.")
+
+    def last_value(self, user, message):
+        if user.zabbix_host is None:
+            self.api.send_message(user.telegram_id, "You are not registered. Register yourself "
+                                                    "using command /register")
+            return
+        args = (message.text or '').split()
+        if len(args) != 2:
+            self.api.send_message(user.telegram_id, self.commands['/last_value'].help)
+            return
+
+        try:
+            api = ZabbixApi(user.zabbix_host, user.zabbix_user, user.zabbix_pass)
+            item = api.get_item_last_value(args[1])
+            text = self._list_of_dict_to_str([item])
+            self.api.send_message(user.telegram_id, text)
+        except RPCException as ex:
+            self.api.send_message(user.telegram_id, "Can't perform an action at zabbix because of "
+                                                    "the reason: {0}".format(ex.message))
+        except RequestException:
+            self.api.send_message(user.telegram_id, "Can't reach zabbix. Are you sure that it is "
+                                                    "available?")
+        except Exception as ex:
+            LOGGER.exception(ex)
+            self.api.send_message(user.telegram_id, "Can't perform an action at zabbix.")
+
+    def _list_of_dict_to_str(self, items):
+        text_list = []
+        for item in items:
+            item_text = '\n'.join("{0}: {1}".format(key, value) for key, value in item.items())
+            text_list.append(item_text)
+        return '\n\n====================\n\n'.join(text_list)
+
+
+
+class TelegramApi(telebot.TeleBot):
+    def send_message(self, chat_id, text, *args, **kwargs):
+        splitted_text = telebot.util.split_string(text, 3000)
+        for text in splitted_text:
+            super().send_message(chat_id, text, *args, **kwargs)
+
+
+api = TelegramApi(settings.TELEGRAM_BOT_API)
 bot = TelegramZabbixBot(api)
